440b2937bd1e9fc8501086b6bf2066767865744a
Gleb <actimel091@gmail.com>
2019-12-25 17:47:41 +0300

    variable

---
+++ js/variable.js
@@ -0,0 +1,11 @@
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin);
+
+let ourPlanetName = "Земля";
+let currentUserName = "John";
+
+const BIRTHDAY = "18.05.1990";
+const age = someCode(BIRTHDAY);
